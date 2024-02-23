2b3e31975be2e1e9ef8a37e97d5eaabca47ef4bb
erindhoxha <hello@erindhoxha.dev>
2024-02-23 17:23:31 +0100

    Validate form

--- scripts/methods.js
+++ scripts/methods.js
@@ -1,6 +1,10 @@
 // Ensure this script runs after the Supabase client has been initialized
-async function insertEmail() {
+async function insertEmail(email) {
   const { error } = await supabase
     .from("subscribers")
-    .insert({ email_address: "erind.cbh@gmail.com" });
+    .insert({ email_address: email });
+
+  return {
+    error,
+  };
 }
--- scripts/validateForm.js
+++ scripts/validateForm.js
@@ -1,21 +1,27 @@
 document
   .getElementById("subscriptionForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
-
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
     var emailInput = document.getElementById("email");
     var message = document.getElementById("subscriptionMessage");
-
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
     if (!emailRegex.test(emailInput.value)) {
-      // If email does not match the pattern, show an error
       message.textContent = "Please enter a valid email address.";
       message.style.display = "block";
       message.style.color = "red";
     } else {
       message.style.display = "none";
-      emailInput.value = ""; // Clear the input
-      insertEmail();
+      const response = await insertEmail(emailInput.value);
+      emailInput.value = "";
+      if (response.error?.code === "23505") {
+        message.textContent = "This email address already exists.";
+        message.style.display = "block";
+        message.style.color = "red";
+      } else if (response.error) {
+        message.textContent =
+          "Something went wrong on our end. Please try again.";
+        message.style.display = "block";
+        message.style.color = "red";
+      }
     }
   });
