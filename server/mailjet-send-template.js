e7add0fe9eafce3794f80e7d673e081edc1454ad
erindhoxha <hello@erindhoxha.dev>
2024-02-29 17:13:47 +0100

    WIP Node scripts

--- server/mailjet-send-template.js
+++ server/mailjet-send-template.js
@@ -1,10 +1,21 @@
 const Mailjet = require("node-mailjet");
 require("dotenv").config();
+const axios = require("axios");
 
 const mailjet = Mailjet.apiConnect(
   process.env.MJ_APIKEY_PUBLIC,
   process.env.MJ_APIKEY_PRIVATE,
 );
+
+const response = axios.get(
+  "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions",
+  {
+    params: { locale: "en-gb", includeAll: "true" },
+    headers: { "Access-Control-Allow-Origin": "*" },
+  },
+);
+
+response.then((res) => console.log(res));
 
 const request = mailjet.post("send", { version: "v3.1" }).request({
   Messages: [
@@ -18,9 +29,17 @@ const request = mailjet.post("send", { version: "v3.1" }).request({
           Email: "erind.cbh@gmail.com",
         },
       ],
+      Variables: {
+        firstName: "Erind",
+        download_url: "https://www.epicgames.com/store/en-US/free-games",
+      },
+      TemplateErrorReporting: {
+        Email: "erind.cbh@gmail.com",
+        Name: "Erind",
+      },
       TemplateID: 5723859,
       TemplateLanguage: true,
-      Subject: "Your email flight plan!",
+      Subject: `Epic Free Games - ${new Date().toDateString()}`,
     },
   ],
 });
