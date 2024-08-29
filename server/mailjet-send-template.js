0c0147d9a201d424bc9e8ef215ec375a82637060
erindhoxha <hello@erindhoxha.dev>
2024-08-29 18:12:24 +0200

    Update script

--- server/mailjet-send-template.js
+++ server/mailjet-send-template.js
@@ -1,22 +1,16 @@
-const Mailjet = require("node-mailjet");
-require("dotenv").config({ path: "../.env" });
-const axios = require("axios");
-const { format } = require("date-fns");
+const Mailjet = require('node-mailjet');
+require('dotenv').config({ path: '../.env' });
+const axios = require('axios');
+const { format } = require('date-fns');
 
 const TEMPLATE_ONE_ROW = 5789055;
 
-const mailjet = Mailjet.apiConnect(
-  process.env.MJ_APIKEY_PUBLIC,
-  process.env.MJ_APIKEY_PRIVATE,
-);
+const mailjet = Mailjet.apiConnect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
 
-const response = axios.get(
-  "https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions",
-  {
-    params: { locale: "en-gb", includeAll: "true" },
-    headers: { "Access-Control-Allow-Origin": "*" },
-  },
-);
+const response = axios.get('https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions', {
+  params: { locale: 'en-gb', includeAll: 'true' },
+  headers: { 'Access-Control-Allow-Origin': '*' },
+});
 
 response
   .then((res) => {
@@ -24,14 +18,15 @@ response
     let template = TEMPLATE_ONE_ROW;
 
     const sortedData = data.elements
-      .map((element, index) => ({ ...element, originalIndex: index })) // add original index to each element
+      .map((element, index) => ({
+        ...element,
+        originalIndex: index,
+      })) // add original index to each element
       .sort((a, b) => {
         const aDiscount =
-          a.promotions?.promotionalOffers?.[0]?.promotionalOffers?.[0]
-            ?.discountSetting?.discountPercentage;
+          a.promotions?.promotionalOffers?.[0]?.promotionalOffers?.[0]?.discountSetting?.discountPercentage;
         const bDiscount =
-          b.promotions?.promotionalOffers?.[0]?.promotionalOffers?.[0]
-            ?.discountSetting?.discountPercentage;
+          b.promotions?.promotionalOffers?.[0]?.promotionalOffers?.[0]?.discountSetting?.discountPercentage;
 
         if (aDiscount === 0 && bDiscount !== 0) {
           return -1;
@@ -52,60 +47,47 @@ response
     const variables = [];
 
     for (let i = 0; i < filteredData.length; i++) {
-      const unfilteredDate =
-        filteredData[i].promotions?.promotionalOffers[0]?.promotionalOffers[0]
-          .endDate;
-      const formattedDate = unfilteredDate
-        ? new Date(unfilteredDate)
-        : undefined;
-      const endDate = formattedDate
-        ? format(formattedDate, "do 'of' MMMM, h a")
-        : undefined;
+      const unfilteredDate = filteredData[i].promotions?.promotionalOffers[0]?.promotionalOffers[0].endDate;
+      const formattedDate = unfilteredDate ? new Date(unfilteredDate) : undefined;
+      const endDate = formattedDate ? format(formattedDate, "do 'of' MMMM, h a") : undefined;
 
       const image =
-        filteredData[i].keyImages.find((img) => img.type === "OfferImageWide")
-          ?.url ||
+        filteredData[i].keyImages.find((img) => img.type === 'OfferImageWide')?.url ||
         filteredData[i].keyImages[2]?.url ||
         filteredData[i]?.keyImages[0]?.url ||
         filteredData[i]?.keyImages[1]?.url;
 
       let title = filteredData[i].title;
 
-      if (title.includes("Mystery Game")) {
-        title = "Mystery Game";
+      if (title.includes('Mystery Game')) {
+        title = 'Mystery Game';
       }
 
       let description = filteredData[i].description;
 
-      if (title.includes("Mystery Game")) {
-        description = "";
+      if (title.includes('Mystery Game')) {
+        description = '';
       }
 
-      const upcomingDate =
-        filteredData[i].promotions?.upcomingPromotionalOffers[0]
-          ?.promotionalOffers[0].startDate;
+      const upcomingDate = filteredData[i].promotions?.upcomingPromotionalOffers[0]?.promotionalOffers[0].startDate;
 
       const upcomingDateFormatted = upcomingDate
         ? `from ${format(new Date(upcomingDate), "do 'of' MMMM, h a")} GMT`
-        : "";
+        : '';
 
       const isFree =
-        filteredData[i].promotions?.promotionalOffers?.[0]
-          ?.promotionalOffers?.[0]?.discountSetting?.discountPercentage === 0;
+        filteredData[i].promotions?.promotionalOffers?.[0]?.promotionalOffers?.[0]?.discountSetting
+          ?.discountPercentage === 0;
 
       const isDiscount =
-        filteredData[i].promotions?.promotionalOffers?.[0]
-          ?.promotionalOffers?.[0]?.discountSetting?.discountPercentage > 0;
+        filteredData[i].promotions?.promotionalOffers?.[0]?.promotionalOffers?.[0]?.discountSetting
+          ?.discountPercentage > 0;
 
-      const price =
-        isFree || isDiscount
-          ? filteredData[i].price?.totalPrice?.fmtPrice.originalPrice
-          : "";
+      const price = isFree || isDiscount ? filteredData[i].price?.totalPrice?.fmtPrice.originalPrice : '';
 
-      let discountedPrice = "";
+      let discountedPrice = '';
       if (isDiscount) {
-        discountedPrice =
-          filteredData[i].price?.totalPrice?.fmtPrice.discountPrice;
+        discountedPrice = filteredData[i].price?.totalPrice?.fmtPrice.discountPrice;
       }
 
       const getDescription = () => {
@@ -115,45 +97,43 @@ response
         if (isDiscount) {
           return `Discounted now until ${endDate} GMT`;
         }
-        if (title.includes("Mystery Game")) {
-          return `Unlocking from ${format(
-            new Date(upcomingDate),
-            "do 'of' MMMM, h a",
-          )} GMT`;
+        if (title.includes('Mystery Game')) {
+          return `Unlocking from ${format(new Date(upcomingDate), "do 'of' MMMM, h a")} GMT`;
         }
         return `Coming soon ${upcomingDateFormatted}`;
       };
 
       const description_2 = getDescription();
 
-      console.log(filteredData[i]);
+      // console.log(filteredData[i]);
 
-      const slugInner =
-        filteredData[i].offerType === "BASE_GAME" || "ADD_ON" ? "p" : "bundles";
+      let slugInner = filteredData[i].offerType === 'BASE_GAME' || 'ADD_ON' ? 'p' : 'bundles';
+
+      if (filteredData[i].offerType === 'BUNDLE') {
+        slugInner = 'bundles';
+      }
 
       const pageSlug =
-        filteredData[i].catalogNs.mappings?.[0]?.pageSlug ||
-        filteredData[i].urlSlug;
+        filteredData[i].productSlug || filteredData[i].catalogNs.mappings?.[0]?.pageSlug || filteredData[i].urlSlug;
 
-      const download_url =
-        isFree || isDiscount
-          ? `https://store.epicgames.com/en-US/${slugInner}/${pageSlug}`
-          : "";
+      const download_url = isFree || isDiscount ? `https://store.epicgames.com/en-US/${slugInner}/${pageSlug}` : '';
+
+      const upcomingDateObj = new Date(upcomingDate);
+      const nextWeek = new Date();
+      nextWeek.setDate(nextWeek.getDate() + 7); // set nextWeek to this time next week
 
       variables.push({
         [`title`]: title,
-        [`description`]: description === title ? "" : description,
+        [`description`]: description === title ? '' : description,
         [`description_2`]: description_2,
         [`image`]: image,
         [`download_url`]: download_url,
-        [`price`]: price === "0" ? "" : price,
-        [`discountedPrice`]: discountedPrice || "",
-        [`isFree`]: isFree ? "true" : "false",
-        [`isDiscount`]: isDiscount ? "true" : "false",
+        [`price`]: price === '0' ? '' : price,
+        [`discountedPrice`]: discountedPrice || '',
+        [`isFree`]: isFree ? 'true' : 'false',
+        [`isDiscount`]: isDiscount ? 'true' : 'false',
       });
     }
-
-    console.log(variables);
 
     sendRequest(template, Object.assign({}, variables));
     // sendTestRequest(template, Object.assign({}, variables));
@@ -165,9 +145,7 @@ const sendRequest = async (templateId, variables) => {
   const limit = 50;
 
   while (true) {
-    const contacts = await mailjet
-      .get(`contact?ContactsList=10422731?limit=${limit}?offset=${offset}`)
-      .request();
+    const contacts = await mailjet.get(`contact?ContactsList=10422731?limit=${limit}?offset=${offset}`).request();
 
     if (contacts.body.Data.length === 0) {
       break;
@@ -177,24 +155,24 @@ const sendRequest = async (templateId, variables) => {
       Email: contact.Email,
     }));
 
-    const request = mailjet.post("send", { version: "v3.1" }).request({
+    const request = mailjet.post('send', { version: 'v3.1' }).request({
       Messages: recipients.map((recipient) => {
         return {
           From: {
-            Email: "hello@epicfreegamesmail.com",
-            Name: "EFGM Newsletter",
+            Email: 'hello@epicfreegamesmail.com',
+            Name: 'EFGM Newsletter',
           },
           To: [recipient],
           Variables: {
             items: variables,
           },
           TemplateErrorReporting: {
-            Email: "erind.cbh@gmail.com",
-            Name: "Erind",
+            Email: 'erind.cbh@gmail.com',
+            Name: 'Erind',
           },
           TemplateID: templateId,
           TemplateLanguage: true,
-          Subject: `Claim games this week - ${new Date().toDateString()}`,
+          Subject: `Claim this week's games - ${new Date().toDateString()}`,
         };
       }),
     });
@@ -216,28 +194,28 @@ const sendRequest = async (templateId, variables) => {
 const sendTestRequest = (templateId, variables) => {
   const recipients = [
     {
-      Email: "erind.cbh@gmail.com",
+      Email: 'erind.cbh@gmail.com',
     },
   ];
 
-  const request = mailjet.post("send", { version: "v3.1" }).request({
+  const request = mailjet.post('send', { version: 'v3.1' }).request({
     Messages: recipients.map((recipient) => {
       return {
         From: {
-          Email: "hello@epicfreegamesmail.com",
-          Name: "EFGM Newsletter",
+          Email: 'hello@epicfreegamesmail.com',
+          Name: 'EFGM Newsletter',
         },
         To: [recipient],
         Variables: {
           items: variables,
         },
         TemplateErrorReporting: {
-          Email: "erind.cbh@gmail.com",
-          Name: "Erind",
+          Email: 'erind.cbh@gmail.com',
+          Name: 'Erind',
         },
         TemplateID: templateId,
         TemplateLanguage: true,
-        Subject: `Free games this week - ${new Date().toDateString()}`,
+        Subject: `Claim this week's games - ${new Date().toDateString()}`,
       };
     }),
   });
