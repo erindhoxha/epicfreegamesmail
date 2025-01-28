aba1286a40221fc0fcd9fd6517d20d3eea8181d9
erindhoxha <hello@erindhoxha.dev>
2025-01-28 12:48:31 +0100

    Fix errors

--- scripts/initialise.js
+++ scripts/initialise.js
@@ -1,4 +1,3 @@
-var supabase = supabase.createClient(
-  CONFIG.SUPABASE_URL,
-  CONFIG.SUPABASE_ANON_KEY,
-);
+import { CONFIG } from "../env.js";
+
+export const supabase = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_ANON_KEY);
--- server/run.js
+++ server/run.js
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY,
-);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 const { data, error } = await supabase.functions.invoke("send-mail-function", {
   body: { name: "Functions" },
