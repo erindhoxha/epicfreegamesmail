3275a477ace9ee4c0a6f35ac7cc0f1b87d9365bd
erindhoxha <hello@erindhoxha.dev>
2025-01-28 12:51:43 +0100

    Init

--- scripts/initialise.js
+++ scripts/initialise.js
@@ -1,3 +1,1 @@
-import { CONFIG } from "../env.js";
-
-export const supabase = supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_ANON_KEY);
+export const supabase = supabase.createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
