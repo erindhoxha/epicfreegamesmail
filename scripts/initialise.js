f49a2d863c3d9d495cc48ec1d6653acc2a0df670
erindhoxha <hello@erindhoxha.dev>
2025-01-28 12:52:39 +0100

    Log

--- scripts/initialise.js
+++ scripts/initialise.js
@@ -1,1 +1,1 @@
-export const supabase = supabase.createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+export const supabaseConfig = supabase.createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
